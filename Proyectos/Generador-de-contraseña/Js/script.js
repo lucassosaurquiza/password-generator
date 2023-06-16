cb4b5d1293fde14707e5d38c991d6219383db05b
Lucas Sosa <lucassosaurquiza97@gmail.com>
2023-06-15 21:56:30 -0300

    Inicializando proyecto

---
+++ Proyectos/Generador-de-contraseña/Js/script.js
@@ -0,0 +1,13 @@
+function generatePassword() {
+  var length = 10; // longitud de la contraseña
+  var charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // caracteres posibles
+  var password = "";
+
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  document.getElementById("password").value = password;
+}
